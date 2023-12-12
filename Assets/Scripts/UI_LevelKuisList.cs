75a2959e1e2004b204169e69217f969108a3fb1a
Ryal Naufal <ryal.naufal@gmail.com>
2023-12-12 19:44:09 +0700

    Mencoba untuk mengimplementasi Trigger Event
    Tapi sulit bro T-T

---
+++ Assets/Scripts/UI_LevelKuisList.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class UI_LevelKuisList : MonoBehaviour
+{
+    [SerializeField]
+    private UI_OpsiLevelKuis _tombolLevel = null;
+
+    [SerializeField]
+    private RectTransform _content = null;
+
+    [SerializeField]
+    private Level_Pack _levelPack = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnloadLevelPack(_levelPack);
+    }
+
+    private void UnloadLevelPack(Level_Pack level_pack)
+    {
+        for (int i = 0; i < level_pack._jumlahLevel; i++)
+        {
+            var t = Instantiate(_tombolLevel);
+            
+            t.SetLevelSoal(level_pack.get_level(i));
+
+            t.transform.SetParent(_content);
+            t.transform.localScale = Vector3.one;
+        }
+    }
+}
--- Assets/Scripts/UI_OpsiLevelKuis.cs
+++ Assets/Scripts/UI_OpsiLevelKuis.cs
@@ -1,9 +1,13 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_OpsiLevelKuis : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private Button _tombol = null;
+
     [SerializeField]
     private TextMeshProUGUI _levelName = null;
 
@@ -16,12 +20,24 @@ public class UI_OpsiLevelKuis : MonoBehaviour
         {
             SetLevelSoal(_levelSoal);
         }
+
+        _tombol.onClick.AddListener(SaatKlik);
+    }
+
+    void OnDestroy()
+    {
+        _tombol.onClick.RemoveListener(SaatKlik);
     }
 
     public void SetLevelSoal(Level_Soal levelSoal)
     {
         _levelName.text = levelSoal.name;
         _levelSoal = levelSoal;
+    }
+
+    private void SaatKlik()
+    {
+        Debug.Log("WKWKWKWK!!!!!!!");
     }
 
 }
